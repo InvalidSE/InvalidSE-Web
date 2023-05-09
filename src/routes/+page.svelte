3a83277edd5d539ab9c75f9d316732b0e1d47df0
InvalidSE <taine.reader@gmail.com>
2023-05-10 10:07:06 +1200

    Progress update
    . not # in the selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,28 +2,48 @@
 
 </svelte:head>
 
-<div class="container">
-    <h1>trolling</h1>
+<div class="title-container">
+    <h1 id="name">Taine Reader</h1>
+    <div id="username-container">
+        <div class="name-underline"/>
+        <h2 id="username">Invalid<i>SE</i></h2>
+        <div class="name-underline"/>
+    </div>
+</div>
+<div class="content">
+    <p>
+        amogusasddkjhaknhfdkjsdhgf
+        kjlasjdhfhksajdhf
+        sdajkfhasdkfhldskhfkjsad
+        jaksdfsafhdsakjjhfskjadh
+    </p>
 </div>
 
+
 <style lang="scss">
-    .container {
+    .title-container {
+        display: flex;
         margin: 0;
         width: 100vw;
         height: 100vh;
-        display: flex;
         justify-content: center;
+        flex-direction: column;
         align-items: center;
-        background-image: url("/blender-logo.png");
+        background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.52), rgba(117, 19, 93, 0.73)), url("/blender-logo.png");
         background-repeat: no-repeat;
         background-position: center;
     }
-
-    :global(body) {
-        margin: 0;
-        padding: 0;
-        font-family: sans-serif;
-        background-color: #121212;
+    #username-container{
+        display: flex;
+        flex-direction: row;
+        align-items: left;
+        justify-content: center;
     }
-
+    .name-underline{
+        width: 250px;
+        height: 12px;
+        background-color: #D0D0D0;
+        box-shadow: 0 0 10px #d0d0d077;
+        margin: 0;
+    }
 </style>
