f5dd804490b67445afd782b73dd57bf7703e3f41
InvalidSE <taine.reader@gmail.com>
2023-05-10 11:00:53 +1200

    Animation go brrr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,28 @@
 <svelte:head>
-
+    <title>Taine Reader</title>
 </svelte:head>
+<script lang="ts">
+    import { onMount } from 'svelte'
+
+    let animate = false
+
+    onMount(() => {
+        animate = true
+    })
+</script>
 
 <div class="title-container">
+    {#if animate}
     <h1 id="name">Taine Reader</h1>
     <div id="username-container">
         <div class="name-underline"/>
         <h2 id="username">Invalid<i>SE</i></h2>
         <div class="name-underline"/>
     </div>
+    {/if}
 </div>
-<div class="content">
-    <p>
-        amogusasddkjhaknhfdkjsdhgf
-        kjlasjdhfhksajdhf
-        sdajkfhasdkfhldskhfkjsad
-        jaksdfsafhdsakjjhfskjadh
-    </p>
-</div>
+
+
 
 
 <style lang="scss">
@@ -38,6 +43,8 @@
         flex-direction: row;
         align-items: left;
         justify-content: center;
+        animation-name: fade_in;
+        animation-duration: 4s;
     }
     .name-underline{
         width: 250px;
@@ -45,5 +52,19 @@
         background-color: #D0D0D0;
         box-shadow: 0 0 10px #d0d0d077;
         margin: 0;
+        animation-name: underline_animation;
+        animation-duration: 2s;
+    }
+    #name {
+        animation-name: fade_in;
+        animation-duration: 4s;
+    }
+    @keyframes underline_animation {
+        0%   {width: 0px;}
+        100% {width: 250px;}
+    }
+    @keyframes fade_in {
+        0%   {opacity: 0;}
+        100% {opacity: 1;}
     }
 </style>
