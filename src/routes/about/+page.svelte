8e7f90dabf955f3e8ad100caa020c5768a324db3
Taine <taine.reader@gmail.com>
2023-05-31 19:52:30 +1200

    e
    ratio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,10 +13,10 @@
     <div class="left-panel"></div>
     <div class="content">
         <div class="about-boxes">
-            <div class="about-box"><p>A</p></div>
-            <div class="about-box"><p>A</p></div>
-            <div class="about-box"><p>A</p></div>
-            <div class="about-box"><p>A</p></div>
+            <div class="history"><p>A</p></div>
+            <div class="interests"><p>A</p></div>
+            <div class="projects"><p>A</p></div>
+            <div class="technology"><p>A</p></div>
 
         </div>
     </div>
@@ -43,15 +43,6 @@
         grid-area: 2 / 1 / 3 / 2; 
     }
 
-    .content {
-        grid-area: 2 / 2 / 3 / 3;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 100%;
-        text-align: center;
-    }
-
     .right-panel { 
         grid-area: 2 / 3 / 3 / 4; 
     }
@@ -62,15 +53,13 @@
     }
 
     .about-boxes {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
         width: 100%;
-
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-auto-rows: auto;
     }
-    .about-box {
-        cursor: pointer;
-        opacity: 1;
+
+    .about-boxes div {
         transform: scale(1);
         transition: 0.25s;
         margin: 8px;
@@ -78,7 +67,23 @@
         background-color: #181818;
         border: rgba(123, 44, 187, 0.541) solid 2px;
         border-radius: 5px;
-        flex-grow: 1;
+        text-align: center;
+    }
+
+    .history{
+        grid-area: 1 / 1 / 2 / 2;
+    }
+    
+    .interests{
+        grid-area: 1 / 2 / 2 / 3;
+    }
+
+    .projects{
+        grid-area: 2 / 1 / 3 / 3;
+    }
+
+    .technology{
+        grid-area: 3 / 1 / 4 / 3;
     }
 
 </style>
