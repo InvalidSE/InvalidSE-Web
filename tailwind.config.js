ad45b73563651fb95f425c54baed1d706334e198
Taine Reader <taine.reader@gmail.com>
2024-07-31 11:40:16 +1200

    Much better start

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 30s ease infinite alternate',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["dark", "dracula", "valentine", "light", "nord"],
+  },
+}
+
