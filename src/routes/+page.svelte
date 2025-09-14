b0c3027d73c6f5bfe5e727931d0384ed054509f0
Taine Reader <taine.reader@gmail.com>
2025-09-14 20:40:57 +1200

    under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,14 +21,20 @@
                 I'm a computer systems engineering student at the University of Auckland
             </span>
         </div>
-        <div class="flex flex-col sm:flex-row lg:flex-row gap-2">
+        <div role="alert" class="alert alert-warning alert-outline">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+        </svg>
+        <span class="text-xl">Website under construction!</span>
+        </div>
+        <!-- <div class="flex flex-col sm:flex-row lg:flex-row gap-2">
             <a href="#about" class="btn btn-outline text-xl">About</a>
             <a href="/projects" class="btn btn-outline text-xl">Projects</a>
             <a href="#contact" class="btn btn-outline text-xl">Contact</a>
-        </div>
+        </div> -->
     </section>
     <div class="spacer my-20" />
-    <section class="content flex flex-col gap-20">
+    <section class="content flex flex-col gap-5">
         <section class="projects">
             <span class="text-5xl font-bold">Highlighted Projects</span>
             <div class="spacer my-4" />
@@ -38,6 +44,7 @@
                 {/each}
             </div>
         </section>
+        <a href="/projects" class="btn btn-outline text-xl">View All</a>
     </section>
 
     
